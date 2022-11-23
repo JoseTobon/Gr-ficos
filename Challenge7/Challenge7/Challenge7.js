a1cc660fe0b7538b5fe22456b7e2c78a31dc3122
JoseTobon <a01411655@itesm.mx>
2022-11-22 20:06:49 -0600

    Challenge7

--- Challenge7/Challenge7/Challenge7.js
+++ Challenge7/Challenge7/Challenge7.js
@@ -4,8 +4,18 @@ function mouseClicked() {
   stage = stage + 1;
 }
 
-function storyboard() {
-  
+function storyboard(x,y) {
+  newX = x;
+  newY = y;
+  stroke(5);
+  point(newX,newY);
+}
+
+function puntitos(a,b) {
+  newA = a;
+  newB = b;
+  stroke(5);
+  point(newA,newB);
 }
 
 function setup() {
@@ -20,8 +30,8 @@ function draw() {
     bezier(120, 40, 320, 60, 320, 300, 330, 300);
     bezier(330, 300, 350, 150, 450, 98, 600, 140);
   } else if (stage == 2) {
-    storyboard();
+    storyboard(100, 50);
   } else if (stage > 2) {
-    puntitos();
+    puntitos(200, 60);
   }
 }
